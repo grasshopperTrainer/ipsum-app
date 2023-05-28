797340576e8e94ae625a6561096d736f9c04b582
dingo.kim <dingo.kim@kakaocorp.com>
2023-05-28 17:16:56 +0900

    add docker-compose for run-dev - gunicorn 을 사용할 시 flask hot reload 작동하지 않지만, 작동하게 할 수 없는 것 같음, flask 를 버릴 것이기 때문에 우회하지 않겠음

--- api/app.py
+++ api/app.py
@@ -1,23 +1,26 @@
+import os
+import yaml
 from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route('/api')
-def hello_camping():
-    return 'hello camping'
-    
-if __name__ == '__main__':
-    import os
-    import yaml
 
-    # config 파일을 불러와서 설정 값 적용시킴
-    # :param APP_PHASE: 앱 환경. README.md 참고할 것
-    #   e.g. run_dev, local_test, prod
-    APP_PHASE = os.environ.get('APP_PHASE', 'run_dev')
-    config_file = f'config_{APP_PHASE}.yml'
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(f'{dir_path}/{config_file}') as f:
-        config = yaml.load(f, yaml.FullLoader)
-    app.config.update(config)
-    
-    app.run(host=config['HOST'], port=config['PORT'])
+@app.route("/api")
+def hello_camping():
+    return "hello camping!"
+
+
+# config 파일을 불러와서 설정 값 적용시킴
+# :param APP_PHASE: 앱 환경. README.md 참고할 것
+#   e.g. run_dev, local_test, prod
+APP_PHASE = os.environ.get("APP_PHASE", "run_dev")
+config_file = f"config_{APP_PHASE}.yml"
+config_path = f"{os.path.dirname(os.path.realpath(__file__))}/{config_file}"
+
+with open(config_path) as f:
+    config = yaml.load(f, yaml.FullLoader)
+app.config.update(config)
+
+if __name__ == "__main__":
+    app.run()

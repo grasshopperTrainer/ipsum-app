ceef5c2e82004c327da6c8597b95a3b519dc94a6
grasshoppertrainer <grasshoppertrainer@gmail.com>
2023-06-03 20:12:05 +0900

    캠프그라운트 카드 추가

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -17,15 +17,11 @@ const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        component: () => import('@/views/OperationPage.vue')
       },
       {
         path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        component: () => import('@/views/ManagementPage.vue')
       }
     ]
   }

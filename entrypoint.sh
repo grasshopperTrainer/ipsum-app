a3775cc6b289c8560c506d3c86e9e7bcbdb03c66
grasshoppertrainer <grasshoppertrainer@gmail.com>
2023-06-06 17:02:18 +0900

    run dev 실행 불가 버그 픽스 - --prefix 빠뜨림

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-npm run dev -- --host ${VUE_DOMAIN} --port ${VUE_PORT} \
+npm run dev --prefix web -- --host ${VUE_DOMAIN} --port ${VUE_PORT} \
 & gunicorn --bind ${GUNICORN_DOMAIN}:${GUNICORN_PORT} --chdir /root/api "app:app" \
 & nginx -g 'daemon off;'
